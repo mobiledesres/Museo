40c5ce5ff8ff64138db62dfce396868be15abe18
mobiledesres <45989029+mobiledesres@users.noreply.github.com>
2026-09-02 23:29:58 +0800

    feat: pack the zip for the full families

--- zip.sh
+++ zip.sh
@@ -3,8 +3,15 @@
 # Run this script to create ZIP archives for each font subset
 
 CURDIR=`dirname "$(realpath "$0")"`
+FONTS_DIR=$CURDIR/fonts
+ZIP_DIR=$CURDIR/zip
 
 cd "$CURDIR"
 
-mkdir -p "$CURDIR/zip"
-find -maxdepth 2 -type d -path './fonts/*' -exec bash -c 'SUBSET=`basename "{}"`; ZIP_FILE='"$CURDIR"'"/zip/${SUBSET}.zip"; cd fonts; zip -r "$ZIP_FILE" "$SUBSET"' \;
+mkdir -p "$ZIP_DIR"
+
+# Pack individual font subsets into separate ZIP archives
+find -maxdepth 2 -type d -path './fonts/*' -exec bash -c 'SUBSET=`basename "{}"`; ZIP_FILE="'"$ZIP_DIR"'/${SUBSET}.zip"; cd "'"$FONTS_DIR"'"; zip -r "$ZIP_FILE" "$SUBSET"' \;
+
+# Pack a whole ZIP
+find "$FONTS_DIR" -maxdepth 1 -type d -name '*' -exec bash -c 'ZIP_FILE="'"$ZIP_DIR"'/Museo font family.zip"; cd "'"$FONTS_DIR"'"; zip -r "$ZIP_FILE" . ' \;
